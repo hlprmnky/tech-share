180cf119a20bbeb37dcb1abef1117259241b7b84
Chris Johnson Bidler <chris@hlprmnky.com>
2015-12-17 23:19:34 -0500

    LV December 2015 5 talk complete

--- src/tech_share/core.clj
+++ src/tech_share/core.clj
@@ -3,4 +3,9 @@
 (defn fizz-buzz
   "Implements the FizzBuzz algorithm"
   [number]
-  number)
+  (condp = 0
+    (mod number 15) "fizzbuzz"
+    (mod number 3) "fizz"
+    (mod number 5) "buzz"
+
+    (str number)))
--- test/tech_share/core_test.clj
+++ test/tech_share/core_test.clj
@@ -3,5 +3,11 @@
   (:use [tech-share.core]))
 
 (facts "About 'fizz-buzz'"
-       (fact "It returns the supplied argument"
-             (= 1 (fizz-buzz 1)) => true))
+       (fact "It returns the string represntation of the argument"
+             (= "1" (fizz-buzz 1)) => true)
+       (fact "It returns 'fizz' when provided an argument divisble by 3"
+             (= "fizz" (fizz-buzz 3)) => true)
+       (fact "It returns 'buzz' when provided an argument divisible by 5"
+             (= "buzz" (fizz-buzz 5)) => true)
+       (fact "It returns 'fizzbuzz ..."
+             (= "fizzbuzz" (fizz-buzz 45)) => true))
